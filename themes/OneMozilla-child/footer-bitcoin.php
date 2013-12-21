35bda26f6596e48ba4c90414dc5e0e0fbfdf4aa6
Mavis Ou <mmmavisss@gmail.com>
2013-12-20 16:47:16 -0800

    add bitcoin donate page

---
+++ themes/OneMozilla-child/footer-bitcoin.php
@@ -0,0 +1,8 @@
+<?php $theme_options = onemozilla_get_theme_options(); ?>
+</div>
+
+<script src="https://www.mozilla.org/tabzilla/media/js/tabzilla.js"></script>
+<?php wp_footer(); ?>
+
+</body>
+</html>
