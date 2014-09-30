1bd96ecb97cc244192bc57f30e18ba1bfbb3c894
Mavis Ou <mmmavisss@gmail.com>
2014-09-30 16:09:01 -0700

    F.M.O - Default Version (without Bootstrap)

--- themes/OneMozilla-child/front-page.php
+++ themes/OneMozilla-child/front-page.php
@@ -9,31 +9,6 @@
     </div>
 
     <div id="dashboard-main" role="main">
-      <div class="row" id="first-row">
-        <section class="white-bg two-columns">
-        <h3>EOY Campaign Total: <span id="period-graph-title"></span></h3>
-        <div class="graph-box-container">
-          <div id="period-graph-container">
-            <div class="spacing-container" data-period-bar-container>
-              <div class="column">
-                <div class="bar"><div class="above-title"></div><div class="below-title"></div></div>
-              </div>
-            </div>
-          </div>
-      </div>
-      </section>
-
-      <section class="white-bg two-columns">
-        <h3>Donations by Source</h3>
-        <div class="graph-box-container">
-          <div id="source-graph-container" data-source-bar-container>
-            <div class="column">
-              <div class="bar"><div class="above-title"></div><div class="below-title"></div></div>
-            </div>
-          </div>
-      </div>
-      </section>
-      </div>
 
       <div class="row" id="second-row">
         <section id="view-blog-section" class="white-bg">
@@ -42,11 +17,6 @@
           <div id="latest-blog-summary">
               <?php get_template_part( 'latest-post' ); ?>
           </div>
-        </section>
-
-        <section id="poll-section" class="white-bg">
-          <h3>You Be the Fundraiser Poll</h3>
-          <?php if ( dynamic_sidebar('polls-widget') ) : else : endif; ?>
         </section>
 
         <section id="older-blog-post-section" class="white-bg">
@@ -65,10 +35,7 @@
 
           <section id="links" class="white-bg two-columns">
             <h3>Links</h3>
-            <a href="//sendto.mozilla.org/page/contribute/EOYFR2013-newdefault?source=viewsource_bottom_link">Donate Today</a><br/>
-            <a href="//fundraising.mozilla.org/campaign-overview/">Campaign Overview</a><br/>
-            <a href="//www.mozilla.org/en-US/about/manifesto/">Mozilla Manifesto</a><br/>
-            <a href="//fundraising.mozilla.org/what-is-view-source-fundraising/">What is View-source Fundraising</a>
+            <?php if ( dynamic_sidebar('links-widget') ) : else : endif; ?>
           </section>
         </div>
       </div>
