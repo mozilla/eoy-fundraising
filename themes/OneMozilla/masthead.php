2435e9419244d2e1affeff181ac4151705b8b8aa
Mavis Ou <mmmavisss@gmail.com>
2014-11-15 12:43:55 -0800

    Update OneMozilla theme (code grabbed from https://github.com/mozilla/One-Mozilla-blog/tree/master/themes/OneMozilla

--- themes/OneMozilla/masthead.php
+++ themes/OneMozilla/masthead.php
@@ -6,7 +6,7 @@
       <h1 id="site-title"><a href="<?php echo esc_url( home_url('/') ); ?>" rel="home" title="<?php _e('Go to the front page', 'onemozilla'); ?>"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a></h1>
     <?php endif; ?>
     <?php if (get_bloginfo('description','display')) : ?>
-      <div id="site-description"><?php echo esc_attr( get_bloginfo('description', 'display') ); ?></div>
+      <h2 id="site-description"><?php echo esc_attr( get_bloginfo('description', 'display') ); ?></h2>
     <?php endif; ?>
     </hgroup>
     <a href="http://www.mozilla.org/" id="tabzilla">Mozilla</a>
