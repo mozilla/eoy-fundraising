f66c72e723b2b1253886e7f3be09b02a0ec03164
Mavis Ou <mmmavisss@gmail.com>
2014-11-19 15:39:20 -0800

    Remove 2013 bitcoin page related files

--- themes/OneMozilla-child/footer-bitcoin.php
+++
@@ -1,8 +0,0 @@
-<?php $theme_options = onemozilla_get_theme_options(); ?>
-</div>
-
-<script src="https://www.mozilla.org/tabzilla/media/js/tabzilla.js"></script>
-<?php wp_footer(); ?>
-
-</body>
-</html>
