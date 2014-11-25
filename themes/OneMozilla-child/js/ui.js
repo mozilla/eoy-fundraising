4966962c838bbd5193a6e45b1c645cd592dd9840
Mavis Ou <mmmavisss@gmail.com>
2014-11-25 11:02:14 -0800

    don't init charts for now

--- themes/OneMozilla-child/js/ui.js
+++ themes/OneMozilla-child/js/ui.js
@@ -1,8 +1,8 @@
 (function() {
 
-  CHARTS.init('#eoy-2014-charts', {
-    donorsByCountry: 'Donors by Country',
-    donorsBySource: 'Donors by Source'
-  }, false);
+  // CHARTS.init('#eoy-2014-charts', {
+  //   donorsByCountry: 'Donors by Country',
+  //   donorsBySource: 'Donors by Source'
+  // }, false);
 
 })();
