cdb851c9a12e5ffa73ffa4561fef3814e3d810a1
Mavis Ou <mmmavisss@gmail.com>
2014-11-27 20:12:02 -0800

    Bug 1105830 - F.M.O - Add date and goal context to the FMO Totalizer

--- themes/OneMozilla-child/totalizer.php
+++ themes/OneMozilla-child/totalizer.php
@@ -5,9 +5,11 @@
       Together, we are building an Internet that puts the public good before profit.
     </h2>
     <div id="totalizer-container">
+      <p>Since November 1 we've raised</p>
       <div id="totalizer">
-        $<div id="amount" class="odometer"></div><small>USD</small>
+        <span class="dollar-sign">$</span><div id="amount" class="odometer"></div><small>USD</small>
       </div>
+      <p>of our year end campaign goal.</p>
     </div>
     <a href="https://sendto.mozilla.org/page/contribute/givenow-1page?preset=2&source=FMO&ref=EOYFR2014&utm_campaign=EOYFR2014&utm_source=FMO&utm_medium=referral&utm_content=FMO_redbutton" target="_blank" class="button button-red">Donate now</a>
   </div>
