a97e0a00684581596552392c38e3bae4dc52869d
Mavis Ou <mmmavisss@gmail.com>
2014-11-28 13:59:26 -0800

    Fix JS error; update totalizer.json

--- themes/OneMozilla-child/js/totalizer-static.js
+++ themes/OneMozilla-child/js/totalizer-static.js
@@ -1,25 +1,28 @@
 (function() {
 
-  var totalizerUI = document.querySelector(".odometer");
+  var totalizerSelector = ".odometer";
+  if ( document.querySelector(totalizerSelector) ) {
+    var totalizerUI = document.querySelector(totalizerSelector);
 
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "wp-content/themes/OneMozilla-child/data/totalizer.json", true);
-  xhr.onerror = function(error) {
-    console.log(error);
-    hideTotalizer();
-  };
-  xhr.onload = function() {
-    var paypalData;
-    try {
-      paypalData = JSON.parse(xhr.responseText);
-      totalizerUI.textContent = paypalData.amount;
-    } catch(e) {
-      console.log(e);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "wp-content/themes/OneMozilla-child/data/totalizer.json", true);
+    xhr.onerror = function(error) {
+      console.log(error);
       hideTotalizer();
-      return;
-    }
-  };
-  xhr.overrideMimeType("application/json");
-  xhr.send();
+    };
+    xhr.onload = function() {
+      var paypalData;
+      try {
+        paypalData = JSON.parse(xhr.responseText);
+        totalizerUI.textContent = paypalData.amount;
+      } catch(e) {
+        console.log(e);
+        hideTotalizer();
+        return;
+      }
+    };
+    xhr.overrideMimeType("application/json");
+    xhr.send();
+  }
 
 })();
