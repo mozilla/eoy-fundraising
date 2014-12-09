41bb4ddc42de04cdf6483c98fd08a73506b62583
Mavis Ou <mmmavisss@gmail.com>
2014-12-09 11:55:22 -0800

    Add graphs in

--- themes/OneMozilla-child/js/ui.js
+++ themes/OneMozilla-child/js/ui.js
@@ -2,7 +2,7 @@
 
   var chartContainerSelector = '#eoy-2014-charts';
   if ( document.querySelector(chartContainerSelector) ) {
-    CHARTS.init('chartContainerSelector', {
+    CHARTS.init(chartContainerSelector, {
       donorsByCountry: 'Donors by Country',
       donorsBySource: 'Donors by Source'
     }, false);
