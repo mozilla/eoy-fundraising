99a0e8a1c76605972b1db977b5baea1818dad9cd
Simon Wex <simon@simonwex.com>
2014-12-24 13:02:51 -0800

    Removing our target goal. Fixing: https://bugzilla.mozilla.org/show_bug.cgi?id=1115390

--- themes/OneMozilla-child/totalizer.php
+++ themes/OneMozilla-child/totalizer.php
@@ -5,15 +5,10 @@
       Together, we are building an Internet that puts the public good before profit.
     </h2>
     <div id="totalizer-container">
-      <p>Since November 1, we've raised</p>
+      <p id="totalizer-copy">Since November 1, we've raised</p>
       <div id="totalizer">
         <span class="dollar-sign">$</span><div id="amount" class="odometer"></div><small>USD</small>
       </div>
-      <p>of our
-        <span id="old-goal">$1.75</span>
-        <span id="new-goal">$2<b>.</b>1 million</span>
-         goal.
-       </p>
     </div>
     <a href="https://sendto.mozilla.org/page/contribute/givenow-seq?preset=2&source=FMO&ref=EOYFR2014&utm_campaign=EOYFR2014&utm_source=FMO&utm_medium=referral&utm_content=FMO_redbutton" target="_blank" class="button button-red">Donate now</a>
   </div>
