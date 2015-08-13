dd23e2e6177dc5f9246b27040b90ad3a4375abb1
Mavis Ou <mmmavisss@gmail.com>
2015-08-13 10:15:47 -0700

    Fixed #15 - remove totalizer

--- themes/OneMozilla-child/totalizer.php
+++ themes/OneMozilla-child/totalizer.php
@@ -1,15 +1,9 @@
 <div id="eoy-home-banner">
   <div class="wrap text-center">
-    <h1>End of Year Campaign</h1>
+    <h1>Mozilla Fundraising</h1>
     <h2>
       Together, we are building an Internet that puts the public good before profit.
     </h2>
-    <div id="totalizer-container">
-      <p id="totalizer-copy">Since November 1, we've raised</p>
-      <div id="totalizer">
-        <span class="dollar-sign">$</span><div id="amount" class="odometer"></div><small>USD</small>
-      </div>
-    </div>
     <a href="https://sendto.mozilla.org/page/contribute/givenow-seq?preset=2&source=FMO&ref=EOYFR2014&utm_campaign=EOYFR2014&utm_source=FMO&utm_medium=referral&utm_content=FMO_redbutton" target="_blank" class="button button-red">Donate now</a>
   </div>
 </div>
