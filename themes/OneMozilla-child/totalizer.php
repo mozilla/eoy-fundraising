030f51338245e0492253f199e536b3ff746aa04b
Ali Al Dallal <ali@alicoding.com>
2015-11-30 12:54:37 -0500

    Update link to donate.mozilla.org

--- themes/OneMozilla-child/totalizer.php
+++ themes/OneMozilla-child/totalizer.php
@@ -4,6 +4,6 @@
     <h2>
       Together, we are building an Internet that puts the public good before profit.
     </h2>
-    <a href="https://sendto.mozilla.org/page/contribute/givenow-seq?preset=2&source=FMO&ref=EOYFR2014&utm_campaign=EOYFR2014&utm_source=FMO&utm_medium=referral&utm_content=FMO_redbutton" target="_blank" class="button button-red">Donate now</a>
+    <a href="https://donate.mozilla.org/en-US/?amount=50&presets=100%2C50%2C25%2C15&source=FMO&ref=EOYFR2015&utm_campaign=EOYFR2015&utm_source=FMO&utm_medium=referral&utm_content=FMO_redbutton" target="_blank" class="button button-red">Donate now</a>
   </div>
 </div>
